db065d6700b152569f6aa78666266ea385fda533
Tharushi Shadeera <tharushishadeera@gmail.com>
2026-06-23 14:44:50 +0530

    Fix API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,5 +7,5 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "https://smart-farming-backend-ddeg.vercel.app/api/v1",
+    "https://smart-farming-backend-ddeg.vercel.app/api",
 });
